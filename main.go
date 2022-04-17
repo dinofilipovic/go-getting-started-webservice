fc8ff3dc9edc1088099afb978e9e4e7da7b076ed
Dino FIlipović <filipovic.dino@gmail.com>
2022-04-17 22:33:08 +0200

    Address of operator
    Usage of address of operator.

--- main.go
+++ main.go
@@ -3,14 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	// Initailizing variable as empty pointer
-	// var firstName *string
-	// initializing pointer as string
-	var firstName *string = new(string)
-	// Tihs is associating into ponter address string "Arthur"
-	*firstName = "Arthur"
-	// This is printing reserved address in CPU memory
+	firstName := "Arthur"
 	fmt.Println(firstName)
-	// This is dereferencing operation
-	fmt.Println(*firstName)
+
+	// Adress of operator
+	ptr := &firstName
+	fmt.Println(ptr, *ptr)
+
+	firstName = "Tricia"
+	fmt.Println(ptr, *ptr)
 }
